17d7a687cf0e4df78799506e1c6177d40cbfe692
tsssni <dingyongyu2002@foxmail.com>
2025-03-22 00:50:22 +0800

    add lambertian bsdf importance sampling

--- flake.nix
+++ flake.nix
@@ -1,32 +1,37 @@
 {
-  description = "blowfish devenv";
+	description = "metatron engine devenv";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-  };
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+	};
 
-  outputs = {
-    nixpkgs
-    , ...
-  }:
-  let
-    setupShell = shell: system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        go
-        hugo
-        nodejs_22
-      ];
+	outputs = {
+		nixpkgs
+		, ...
+	}:
+	let
+		lib = nixpkgs.lib;
 
-      shellHook = ''
-        exec ${shell}
-      '';
-    };
-  in {
-    devShells."aarch64-darwin".default = setupShell "zsh" "aarch64-darwin";
-    devShells."x86_64-linux".default = setupShell "elvish" "x86_64-linux";
-  };
+		systems = [
+			"aarch64-darwin"
+			"x86_64-linux"
+		];
+
+		devShells = systems
+			|> lib.map (system:
+				let
+					pkgs = import nixpkgs {
+						inherit system;
+					};
+				in {
+					"${system}".default = pkgs.mkShellNoCC {
+						packages = with pkgs; [
+							hugo
+							nodejs_22
+						];
+					};
+				}
+			)
+			|> lib.mergeAttrsList;
+	in { inherit devShells; };
 }
